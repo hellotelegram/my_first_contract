1452a59acc60161827ca2b543025cf9859b56e2a
helloword <>
2024-12-03 09:04:58 +0800

    增加了计数器

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -5,20 +5,28 @@ import { MainContract } from "../wrappers/MainContract";
 import "@ton/test-utils";
 
 describe("main.fc contract tests", () => {
-    it("should get the proper most recent sender address", async () => {
+    it("should successfully increase counter in contract and get the proper most recent sender address", async () => {
         const blockchain = await Blockchain.create();
         const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
 
+        const initAddress = await blockchain.treasury("initAddress");
+
         const myContract = blockchain.openContract(
-            await MainContract.createFromConfig({}, codeCell)
+            await MainContract.createFromConfig(
+                {
+                    number: 0,
+                    address: initAddress.address,
+                },
+                codeCell
+            )
         );
 
-        // 创建一个sender钱包(钱包是合约的一种)
         const senderWallet = await blockchain.treasury("sender");
-        // 发送一个内部消息 (使用此方法时我们不必担心传递 ContractProvider，它将作为合约实例内置功能的一部分在底层传递)
-        const sentMessageResult = await myContract.sendInternalMessage(
+
+        const sentMessageResult = await myContract.sendIncrement(
             senderWallet.getSender(),
-            toNano("0.05")
+            toNano("0.05"),
+            2
         );
 
         expect(sentMessageResult.transactions).toHaveTransaction({
@@ -30,5 +38,6 @@ describe("main.fc contract tests", () => {
         const data = await myContract.getData();
 
         expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+        expect(data.number).toEqual(2);
     });
 });
--- wrappers/MainContract.ts
+++ wrappers/MainContract.ts
@@ -1,6 +1,14 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 // 实现并导出我们合约的包装器
 
+export type MainContractConfig = {
+    number: number;
+    address: Address;
+}
+export function mainContractConfigToCell(config: MainContractConfig): Cell {
+    return beginCell().storeUint(config.number, 32).storeAddress(config.address).endCell();
+}
+
 export class MainContract implements Contract {
     constructor(
         readonly address: Address,
@@ -8,30 +16,43 @@ export class MainContract implements Contract {
     ) { }
 
     // 从配置文件中创建合约
-    static createFromConfig(config: any, code: Cell, workchain = 0) {
-        const data = beginCell().endCell();
+    static createFromConfig(
+        config: MainContractConfig,
+        code: Cell,
+        workchain = 0
+    ) {
+        const data = mainContractConfigToCell(config);
         const init = { code, data };
         const address = contractAddress(workchain, init);
 
         return new MainContract(address, init);
     }
 
-    async sendInternalMessage(
-        // provider 会自动传递
+    async sendIncrement(
         provider: ContractProvider,
         sender: Sender,
-        value: bigint
+        value: bigint,
+        increment_by: number
     ) {
+
+        const msg_body = beginCell()
+            .storeUint(1, 32) // OP code
+            .storeUint(increment_by, 32) // increment_by value
+            .endCell();
+
         await provider.internal(sender, {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
+            body: msg_body,
         });
     }
+
+
     async getData(provider: ContractProvider) {
-        const { stack } = await provider.get("get_the_latest_sender", []);
+        const { stack } = await provider.get("get_contract_storage_data", []);
         return {
-          recent_sender: stack.readAddress(),
+            number: stack.readNumber(),
+            recent_sender: stack.readAddress(),
         };
     }
 }
